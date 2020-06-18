4a78e666c16530fd1fb0f654014ad4d0f5d15e8b
M4cs <mabridgland@protonmail.com>
2020-06-18 00:42:08 -0400

    Add project files.

---
+++ ModStats.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System;
+
+namespace mStats
+{
+    public class ModStats
+    {
+        public void Init(MonoBehaviour mod, int modId)
+        {
+            CheckForUUID();
+        }
+
+        private async void CheckForUUID()
+        {
+            if (!Directory.Exists(Directory.GetCurrentDirectory() + "..\\mStats\\"))
+            {
+                Directory.CreateDirectory(Directory.GetCurrentDirectory() + "..\\mStats\\");
+                var client = new HttpClient();
+                HttpResponseMessage response = new HttpResponseMessage();
+                try
+                {
+                    var task = client.GetAsync("http://localhost:5000/get-uuid");
+                    task.Wait();
+                    response = task.Result;
+                } catch (Exception)
+                {
+                    response.StatusCode = HttpStatusCode.RequestTimeout;
+                }
+                var newTask = response.Content.ReadAsStringAsync();
+                newTask.Wait();
+                string uuid = newTask.Result;
+                using (StreamWriter writer = File.CreateText(Directory.GetCurrentDirectory() + "..\\mStats\\mStats.txt")){
+                    await writer.WriteAsync(uuid);
+                }
+            }
+
+        }
+
+    }
+}
